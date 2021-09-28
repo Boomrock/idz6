ef682f96b7659b35d62fd11614b5c77c21c96050
Boomrock <63188345+Boomrock@users.noreply.github.com>
2021-09-28 19:34:57 +0700

    main

---
+++ idz6/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace idz6
+{
+    public partial class Form1 : Form
+    {
+
+        private string DefaultString, ResultString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DefaultString = textBox1.Text;
+            ResultString = "";
+            int i = 0,
+                n = 0;
+            do
+            {
+                if (DefaultString[i] == ' ')
+                {
+                    
+                    ResultString += DefaultString[i];
+                    i++;
+                }
+                else
+                {
+
+                    ResultString += DefaultString[i];
+                }
+                i++;
+            } while (i < DefaultString.Length);
+            textBox1.Text = ResultString;
+        }
+    }
+}
